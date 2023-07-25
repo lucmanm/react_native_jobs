c53167ec709ef3f0a87b9d5ad60de73d04a5454e
lucmanm <greenjamez22@gmail.com>
2023-07-26 00:22:21 +0300

    ...

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,29 +1,93 @@
-import { Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from "react-native"
-import { Stack, useRouter, useSearchParams } from "expo-router"
-import { useCallback, useState } from "react"
-import {Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from "../../components"
-import {COLORS,icons, Sizes} from "../../constants"
-import useFetch from "../../hook/useFetch"
+import {
+    Text,
+    View,
+    SafeAreaView,
+    ScrollView,
+    ActivityIndicator,
+    RefreshControl,
+} from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+import {
+    Company,
+    JobAbout,
+    JobFooter,
+    JobTabs,
+    ScreenHeaderBtn,
+    Specifics,
+} from "../../components";
+import { COLORS, icons, SIZES, Sizes } from "../../constants";
+import useFetch from "../../hook/useFetch";
 
 const JobDetails = () => {
-    const params = useSearchParams()
-    const router = useRouter()
+    const params = useSearchParams();
+    const router = useRouter();
 
-    const {data, isLoading, error, refetch } =useFetch('job-details', {
-        job_id: params.id
-    })
-    
-  return (
-    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
-    <Stack.Screen options={{
-        headerStyle: {backgroundColor: COLORS.lightWhite},
-        headerShadowVisible: false,
-        headerBackVisible: false,
-        
-    }}></Stack.Screen>
+    const { data, isLoading, error, refetch } = useFetch("job-details", {
+        job_id: params.id,
+    });
 
-    </SafeAreaView>
-  )
-}
+    const [refreshing, setRefreshing] = useState(false);
 
-export default JobDetails
+    const onRefresh = () => {};
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerBackVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn
+                            iconsUrl={icons.left}
+                            dimension="60%"
+                            handlePress={() => router.back()}
+                        />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn
+                            iconsUrl={icons.share}
+                            dimension="60%"
+                        />
+                    ),
+                    headerTitle: "",
+                }}>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={refreshing}
+                            onRefresh={onRefresh}
+                        />
+                    }>
+                    {isLoading ? (
+                        <ActivityIndicator
+                            size="large"
+                            color={COLORS.primary}
+                        />
+                    ) : error ? (
+                        <Text>Something went Wrong</Text>
+                    ) : data.length === 0 ? (
+                        <Text> No Data</Text>
+                    ) : (
+                        <View
+                            style={{
+                                padding: SIZES.medium,
+                                paddingBottom: 100,
+                            }}>
+                            <Company
+                                comapanyLogo={data[0].employer_logo}
+                                jobTitle={data[0].jobTitle}
+                                companyName={data[0].employer_name}
+                                location={data[0].job_country}
+                            />
+                            <JobTabs />
+                        </View>
+                    )}
+                </ScrollView>
+            </Stack.Screen>
+        </SafeAreaView>
+    );
+};
+
+export default JobDetails;
