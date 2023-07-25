00af04b8442b0bed3d30c1ba9ffcc5690823f9e9
lucmanm <greenjamez22@gmail.com>
2023-07-26 01:27:20 +0300

    ...

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -16,8 +16,10 @@ import {
     ScreenHeaderBtn,
     Specifics,
 } from "../../components";
-import { COLORS, icons, SIZES, Sizes } from "../../constants";
+import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
+
+const tabs = ["About", "Qualification", "Responsibilities"];
 
 const JobDetails = () => {
     const params = useSearchParams();
@@ -28,8 +30,28 @@ const JobDetails = () => {
     });
 
     const [refreshing, setRefreshing] = useState(false);
-
+    const [activeTab, setActiveTab] = useState(tabs[0]);
     const onRefresh = () => {};
+
+    const displayTabContent = () => {
+        console.log(data[0].job_highlights?.Qualifications);
+        switch (activeTab) {
+            case "Qualification":
+                return (
+                    <Specifics
+                        title="Qualification"
+                        points={
+                            data[0].job_highlights?.Qualifications ?? ["N/A"]
+                        }
+                    />
+                );
+            case "About":
+            case "Responsibilities":
+            default:
+                break;
+        }
+    };
+
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
             <Stack.Screen
@@ -39,53 +61,55 @@ const JobDetails = () => {
                     headerBackVisible: false,
                     headerLeft: () => (
                         <ScreenHeaderBtn
-                            iconUrl={icons.left}
+                            iconsUrl={icons.left}
                             dimension="60%"
                             handlePress={() => router.back()}
                         />
                     ),
                     headerRight: () => (
                         <ScreenHeaderBtn
-                            iconUrl={icons.share}
+                            iconsUrl={icons.share}
                             dimension="60%"
                         />
                     ),
                     headerTitle: "",
-                }}>
-                <ScrollView
-                    showsVerticalScrollIndicator={false}
-                    refreshControl={
-                        <RefreshControl
-                            refreshing={refreshing}
-                            onRefresh={onRefresh}
+                }}
+            />
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }>
+                {isLoading ? (
+                    <ActivityIndicator size="large" color={COLORS.primary} />
+                ) : error ? (
+                    <Text>Something went Wrong</Text>
+                ) : data.length === 0 ? (
+                    <Text> No Data</Text>
+                ) : (
+                    <View
+                        style={{
+                            padding: SIZES.medium,
+                            paddingBottom: 100,
+                        }}>
+                        <Company
+                            companyLogo={data[0].employer_logo}
+                            jobTitle={data[0].job_title}
+                            companyName={data[0].employer_name}
+                            location={data[0].job_country}
                         />
-                    }>
-                    {isLoading ? (
-                        <ActivityIndicator
-                            size="large"
-                            color={COLORS.primary}
+                        <JobTabs
+                            tabs={tabs}
+                            activeTab={activeTab}
+                            setActiveTab={setActiveTab}
                         />
-                    ) : error ? (
-                        <Text>Something went Wrong</Text>
-                    ) : data.length === 0 ? (
-                        <Text> No Data</Text>
-                    ) : (
-                        <View
-                            style={{
-                                padding: SIZES.medium,
-                                paddingBottom: 100,
-                            }}>
-                            <Company
-                                companyLogo={data[0].employer_logo}
-                                jobTitle={data[0].job_title}
-                                companyName={data[0].employer_name}
-                                location={data[0].job_country}
-                            />
-                            <JobTabs />
-                        </View>
-                    )}
-                </ScrollView>
-            </Stack.Screen>
+                        {displayTabContent()}
+                    </View>
+                )}
+            </ScrollView>
         </SafeAreaView>
     );
 };
